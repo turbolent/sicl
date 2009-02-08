a0ff055d6344e5295d698e25d99487585874467b
Bastian Müller <bastian@turbolent.com>
2009-02-08 21:53:33 +0100

    license

--- package.lisp
+++ package.lisp
@@ -1,11 +1,20 @@
-;;;;
-;;;; SPARQL processor
-;;;; v. 0.4.0
-;;;; Copyright 2005 Richard Newman
-;;;; Copyright 2008 Bastian Müller
-;;;;
-;;;;
-;;;; <http://www.w3.org/TR/rdf-sparql-query/>
+;;
+;; Copyright 2009 Bastian Mueller 
+;;
+;; This file is part of sicl.
+;;
+;; sicl is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; sicl is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with sicl.  If not, see <http://www.gnu.org/licenses/>.
 
 (defpackage :sicl
   (:export :parse-sparql)
